cecd0bfdef0c69b37a5345217fab0fb4c17a8a19
rodVieira021 <rodvieira021@gmail.com>
2022-04-26 20:34:56 +0300

    emailbtn

--- src/comp/Info.js
+++ src/comp/Info.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import photo from '../comp/imgs/perfil.png';
+import Photo from './imgs/perfil.png';
+import SvgEmail from './imgs/maillogo.svg';
 
 function Info() {
   return (
     <div className="info">
       <div className="container-info">
         <div className="perfil-container">
-          <img src={photo} alt="perfil" className="perfil" />
+          <img src={Photo} alt="perfil" className="perfil" />
         </div>
         <div className="text">
           <h2>Laura Smith</h2>
           <h3>Frontend Developer</h3>
           <h4>laurasmith.web</h4>
-          <button className="btn btn--1">Email</button>
+          <button className="btn btn--1">
+            <img
+              className="btn-email"
+              src={SvgEmail}
+              width="25px"
+              alt="svgEmail"
+            />
+            Email
+          </button>
           <button className="btn btn--2">LinkedIn</button>
         </div>
       </div>
